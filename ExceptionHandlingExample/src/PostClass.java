58cb7ef827eb5e1280633b6ba639300237c95f84
jafar95 <jafar.sadik@northsouth.edu>
2021-01-06 05:21:34 +0600

    Custom Exception example added

---
+++ ExceptionHandlingExample/src/PostClass.java
@@ -0,0 +1,143 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JrBcrypted
+*  Md. Jafar Sadik
+*  ID: 1620660042
+ */
+ public class PostClass extends ExceptionConditionImplementations
+    {
+        public static void validatePost(String text, int d, int m, String y) throws TextLengthLimitExceedsException, InvalidInputDateException
+        {
+            if(doesTextLengthLimitExceed(text)== true)
+                throw new TextLengthLimitExceedsException("The text lenght cannot be greater than 220 characters.");
+             else if(isValidDate(d, m, y) == false)
+            {
+                throw new InvalidInputDateException("Failed! Date Format is not valid!");
+            }
+            else
+                System.out.println("Posted successfully!\n[Post Text]: " + text + "\nDate Posted: " + dateString);
+        }
+        public static void validatePost(String text, String file, int d, int m, String y) throws TextLengthLimitExceedsException, ScammingFileException,
+                CorruptedFilesException, VirusFilesException, VirusFilesException, InvalidInputDateException
+        {
+            if(doesTextLengthLimitExceed(text)== true)
+            {
+                throw new TextLengthLimitExceedsException("The text lenght cannot be greater than 220 characters.");
+            }
+                
+            else if(isScamFile(file)== true)
+            {
+                throw new ScammingFileException("Failed! The file seems to contain something that is scam!");
+            }
+            else if(isVirusFile(file) == true)
+            {
+                throw new VirusFilesException("Failed! The file seems to contain some virus.");
+            }
+            else if(isCorruptedFile(file) == true)
+            {
+                throw new CorruptedFilesException("Failed! The file seems to be corrupted or broken!");
+            }
+            else if(isValidDate(d, m, y) == false)
+            {
+                throw new InvalidInputDateException("Failed! Date Format is not valid!");
+            }
+            else
+                System.out.println("Posted Successfully!\n[Post Text]: "+ text +"\nAttached File: " + file + "\nDate Posted: " + dateString);
+            
+              
+        }
+                
+        
+        
+        public static void main(String[] args)
+        {
+             String inp, text, fileName, yyyy;
+             int dd, mm;
+             Scanner input = new Scanner(System.in);
+             while(true)
+             {
+                 System.out.println("\n\nWelcome to the System!\nPlease Press '1' only to Post the text with date."
+                     + "\nPress '2' Post the text with attachment file and date.\nPress 'x' to exit this prompt!");
+                 inp = input.nextLine();
+                 if("1".equals(inp))
+                 {
+                     System.out.println("\nEnter the Post Text: ");
+                     text = input.nextLine();
+                     //input.nextLine();
+                     System.out.println("\nEnter the day of the Post date: ");
+                     dd = input.nextInt();
+                     System.out.println("\nEnter the month number: ");
+                     mm = input.nextInt();
+                     System.out.println("\nEnter the year(yyyy): ");
+                     yyyy = input.next();
+                     input.nextLine();
+                     
+                     try
+                     {
+                        validatePost(text, dd, mm, yyyy);    
+                     }
+                     catch(Exception e)
+                     {
+                         System.out.println("\nException Occured: " +e);
+                     }
+                     
+                 }
+                 else if("2".equals(inp))
+                 {
+                     System.out.println("\nEnter the Post Text: ");
+                     text = input.nextLine();
+                     System.out.println("\nEnter the day of the Post date: ");
+                     dd = input.nextInt();
+                     System.out.println("\nEnter the month number: ");
+                     mm = input.nextInt();
+                     System.out.println("\nEnter the year(yyyy): ");
+                     yyyy = input.next();
+                     System.out.println("\nEnter the filename you want to attach: ");
+                     fileName = input.next();
+                     
+                     
+                     try
+                     {
+                        validatePost(text, fileName, dd, mm, yyyy);    
+                     }
+                     catch(Exception e)
+                     {
+                         System.out.println("\nException Occured: " +e);
+                     }
+                 }
+                 else if("x".equals(inp))
+                 {
+                    break;   
+                 }
+                 
+             }
+        }
+                
+    }
+/* text1 (less than 220 characters): The Quick Brown Fox Jumps over the lazy dog */
+/* text2 (greater than 220 characters): The Quick Brown Fox Jumps over the lazy dog
+The Quick Brown Fox Jumps over the lazy dog The Quick Brown Fox Jumps over the lazy dog The Quick Brown Fox Jumps over the lazy dog 
+The Quick Brown Fox Jumps over the lazy dog
+The Quick Brown Fox Jumps over the lazy dog The Quick Brown Fox Jumps over the lazy dog The Quick Brown Fox Jumps over the lazy dog
+The Quick Brown Fox Jumps over the lazy dog
+The Quick Brown Fox Jumps over the lazy dog The Quick Brown Fox Jumps over the lazy dog The Quick Brown Fox Jumps over the lazy dog
+The Quick Brown Fox Jumps over the lazy dog
+The Quick Brown Fox Jumps over the lazy dog The Quick Brown Fox Jumps over the lazy dog The Quick Brown Fox Jumps over the lazy dog
+*/
+    
+    
+    
+
+
+
+
+
---
+++ ExceptionHandlingExample/src/ScammingFileException.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author JrBcrypted
+*  Md. Jafar Sadik
+*  ID: 1620660042
+ */
+import java.util.ArrayList;
+import java.util.Scanner;
+class ScammingFileException extends Exception
+{
+    ScammingFileException(String s)
+    {
+        super(s);
+    }
+}
+ class CorruptedFilesException extends Exception
+{
+   CorruptedFilesException(String s)
+   {
+       super(s);
+   }
+}
+class VirusFilesException extends Exception
+{
+    VirusFilesException(String s)
+    {
+        super(s);
+    }    
+}
+class TextLengthLimitExceedsException extends Exception
+{
+    TextLengthLimitExceedsException(String s)
+    {
+        super(s);
+    }
+}
+class InvalidInputDateException extends Exception
+{
+    InvalidInputDateException(String s)
+    {
+        super(s);
+    }
+}
+class ExceptionConditionImplementations
+{
+    private static String[] scamFiles = {"scamfile1.pdf", "scamfile2.dll", "scamfile3.cfx", "scamfile4.exe"};
+    private static String[] corruptedFiles = {"borkenFile.mp4", "brokenFile.mp3", "corruptedFile.3gp"};
+    private static String[] virusFiles = {"virus1.exe", "virus2.dll", "virus3.cmd"};
+    static int dd, mm;
+    static String yyyy;
+    public static String dateString;
+    
+   
+    public static boolean isScamFile(String scamCheck)
+    {
+      boolean ret = true;
+        for (int i = 0; i < scamFiles.length; i++)
+        {
+            if (scamFiles[i] == scamCheck)
+            {
+                ret = true;
+                break;
+                
+            }
+            else 
+                ret = false;
+            
+        }
+        return ret;
+    }
+    public static boolean isCorruptedFile(String corruptedCheck)
+    {
+      boolean ret = true;
+        for (int i = 0; i < corruptedFiles.length; i++)
+        {
+            if (corruptedFiles[i] == corruptedCheck)
+            {
+                ret = true;
+                break;
+                
+            }
+            else 
+                ret = false;
+            
+        }
+        return ret;
+    }
+    public static boolean isVirusFile(String virusCheck)
+    {
+      boolean ret = true;
+        for (int i = 0; i < virusFiles.length; i++)
+        {
+            if (virusFiles[i] == virusCheck)
+            {
+                ret = true;
+                break;
+                
+            }
+            else 
+                ret = false;
+            
+        }
+        return ret;
+    }
+    public static boolean doesTextLengthLimitExceed(String text)
+    {
+        boolean ret;
+        if(text.length()>220)
+        {
+            ret = true;
+        }
+        else
+            ret = false;
+        return ret;
+    }
+    public static boolean isValidDate(int dd, int mm, String yyyy)
+    {
+        boolean ret = false;
+        //boolean dateValid = false;
+        //boolean monthValid 
+        if((dd>=1 || dd<=31) && (mm>=1 || mm<=12) && yyyy.length()==4)
+        {
+            dateString = dd + "-" + mm + "-" + yyyy;
+            ret = true;
+            
+        }
+        else
+            ret = false;
+        
+        return ret;
+    }
+}
+    
+    
